b3c8f7aa8f617bdc22b13f2c50e5095df3690ae9
samueljsluo <xlottoxitouch@gmail.com>
2022-05-14 00:53:08 +0800

    Update DeleteNodeInABST.py

--- Median/450. Delete Node in a BST/DeleteNodeInABST.py
+++ Median/450. Delete Node in a BST/DeleteNodeInABST.py
@@ -13,22 +13,23 @@ class Solution:
             root.left = self.deleteNode(root.left, key)
         else:
             new_root = TreeNode()
-            if root.left and root.right:
+            if root.left and root.right:  # if there have both right and left node, find smallest node in the right side
                 parent = root
-                new_root = root.right
+                new_root = root.right  # locate to the smallest node, and keep track of the parent node
                 while new_root.left:
                     parent = new_root
                     new_root = new_root.left
                 
                 if parent != root:  # if there are more than one child on the right side
                     parent.left = new_root.right
-                    new_root.right = root.right  # if this line out side of if function there would be circle in TreeNode
+                    new_root.right = root.right  # if place this line outside of if function there would be circle in TreeNode
                 new_root.left = root.left
                 
                 del root
                 return new_root
-            elif not root.left:
+            elif not root.left:  # if there is no left of the node, return right
                 return root.right
-            elif not root.right:
+            elif not root.right:  # if there is no right of the node, return left
                 return root.left
         return root
+        
